9d74f9d7ff5651bc32cf993af1c277b2cd195d0a
spidycoder <imh10028.21@bitmesra.ac.in>
2023-10-02 23:14:55 +0530

    Revert "first commit"
    This reverts commit 8fd249bb03dbebebf4b7192e5a0901563dbae3a3.

--- lib/validations/user.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod";
-
-export const UserValidate = z.object({
-  profile_photo: z.string().url().nonempty(),
-  name: z.string().min(3).max(20),
-  username: z.string().min(3).max(20),
-  bio: z.string().min(3).max(1000),
-});
